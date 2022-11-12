1681583eadc166a9386310fe62add574fa6b67ef
ange melissa <nguessanmelissa01@gmail.com>
2022-11-12 15:55:14 +0000

    gestion d'erreur

--- gestion_erreur1.py
+++ gestion_erreur1.py
@@ -1,6 +1,17 @@
-nombre=input("entrez le nombre:")
+# nombre=input("entrez le nombre:")
+
+# try:
+#     nombre=int(nombre)
+# except ValueError as e:
+#     print(e)
+
 
 try:
-    nombre=int(nombre)
-except ValueError as e:
-    print(e)
+    numerateur = int(input("Entrez un numérateur : "))
+    denominateur = int(input("Entrez un dénominateur : "))
+    resultat = numerateur / denominateur
+    print("Le resultat de la division est", resultat)
+except (ValueError, ZeroDivisionError):
+    print("Désolé, quelque chose ne s'est pas bien passé.")
+else:
+    print(resultat)
