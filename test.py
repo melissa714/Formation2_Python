6394463c13b8303a761199e895b8213caa45a0c7
ange melissa <nguessanmelissa01@gmail.com>
2022-11-13 11:47:22 +0000

    time

--- test.py
+++ test.py
@@ -1,17 +1,28 @@
-import sys  
-from sys import version
+# import sys  
+# from sys import version
+# import random
+# import time as t  
+
+# date=t.localtime()
+
+# print(date)
+# print(sys.version)
+
+# time_now=t.localtime()
+# print("transaction effectué à:",str(time_now.tm_hour) + "h" )
+
+# liste=[1,3,5,8,9,5]
+# print(random.choice(liste))
+
+# print(version)
+
+import sys 
 import random
-import time as t  
+import time
+from datetime import date,time
+from datetime import datetime
 
-date=t.localtime()
-
-print(date)
-print(sys.version)
-
-time_now=t.localtime()
-print("transaction effectué à:",str(time_now.tm_hour) + "h" )
-
-liste=[1,3,5,8,9,5]
-print(random.choice(liste))
-
-print(version)
+# print(sys.path)
+print(date.today())
+print(datetime.now())
+print(random.randint(1,20))
